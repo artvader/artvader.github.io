8f3175834814a92263e2e08f041ab45870a35740
Art <art.jazmines@xuenn.com>
2020-06-30 17:18:42 +0800

    added dist folder

---
+++ dist/precache-manifest.62599460972ee75c38c59678ee0f6521.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a28c2faeaa94a8e2ec24",
+    "url": "/css/app.cd22937f.css"
+  },
+  {
+    "revision": "aba88e40e56bd66b6ba737d22f985edb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3145e9c45af20118162",
+    "url": "/js/about.28bc98ae.js"
+  },
+  {
+    "revision": "a28c2faeaa94a8e2ec24",
+    "url": "/js/app.0f9dbfca.js"
+  },
+  {
+    "revision": "29c5c47ab11f214f6d66",
+    "url": "/js/chunk-vendors.75975ebd.js"
+  },
+  {
+    "revision": "cc7b175a179d3da90c961b171cde826f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
